d14686ae06ce5116254685636f39f7896edbcebd
Артем Горшков <a.gorshkov@ANSEREM.local>
2023-08-22 18:00:11 +0300

    abc

--- GoGoProject/ClientApp/src/AppRoutes.js
+++ GoGoProject/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import RankItem from "./components/RankItem";
 
 const AppRoutes = [
   {
@@ -14,7 +15,11 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
-  }
+    },
+    {
+        path: '/rank-item',
+        element: <RankItem />
+    }
 ];
 
 export default AppRoutes;
---
+++ GoGoProject/Models/ItemModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+namespace GoGoProject.Models
+{
+    public abstract class ItemModel
+    {
+        public int Id { get; set; }
+        public abstract string Name { get; set; }
+
+    }
+
+    public class TypeIP : ItemModel
+    {
+        public override string Name 
+        { 
+            get { return "IP"; }
+            set { Name = value; }
+             
+        } //{ get; set = "IP"; } 
+    }
+
+    public class TypeOOO : ItemModel
+    {
+        public override string Name 
+        { 
+            get { return "OOO"; }
+            set { Name = value; }
+        }
+    }
+}
