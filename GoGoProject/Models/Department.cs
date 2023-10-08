0868d3148d0306adb6d80d4bfc9bdb9ee771b4a3
Artmee <rappil11051999@gmail.com>
2023-10-08 17:32:01 +0300

    hello again

---
+++ GoGoProject/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿namespace CompanyProjectVer2.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Guid ParentDepartmentID { get; set; }
+
+        public Department(int id, string name, string code, Guid parentDepartmentID) 
+        {
+            Id = id;
+            Name = name;
+            Code = code;
+            ParentDepartmentID = parentDepartmentID;
+        }
+        //
+    }
+}
---
+++ GoGoProject/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace CompanyProjectVer2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public Guid DepartmentId { get; set; }
+        public string SurName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public string DocSeries { get; set; }
+        public string DocNumber { get; set; }
+        public string Position { get; set; }
+        //
+        public Employee(int id, Guid departmentId, string surName, string firstName, string patronymic, DateTime dateOfBirth, string docSeries, string docNumber, string position)
+        {
+            Id = id;
+            DepartmentId = departmentId;
+            SurName = surName;
+            FirstName = firstName;
+            Patronymic = patronymic;
+            DayOfBirth = dateOfBirth;
+            DocSeries = docSeries;
+            DocNumber = docNumber;
+            Position = position;
+        }
+
+
+    }
+}
